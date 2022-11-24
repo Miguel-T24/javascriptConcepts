7e7db4138085ad0e92cd1c5a3b230cc0f2b9d6e9
Miguel-T24 <miguelt2296@gmail.com>
2022-11-23 19:54:22 -0500

    add stopPropagation and preventDefault

--- Javascript - Concepts/web-apis/stopPropagation&preventDefault.js
+++ Javascript - Concepts/web-apis/stopPropagation&preventDefault.js
@@ -1,2 +1,18 @@
 'use strict'
 
+// Stop Propagation
+
+const $divEventos = document.querySelectorAll('.eventos-flujo div'),
+$linkEvent = document.querySelector(".eventos-flujo a");
+function flujoEventos(e){
+    console.log(`Hola te saluda:  ${this.className}, el click lo origino: ${e.target.className}`);
+    e.stopPropagation();
+}
+$divEventos.forEach((div) => {
+    div.addEventListener("click" , flujoEventos);
+});
+
+$linkEvent.addEventListener("click" , e =>{
+    alert("Soy Jon Mircha");
+    e.preventDefault(); //cancel action that have the element by default, in this case we cancel the link
+})
