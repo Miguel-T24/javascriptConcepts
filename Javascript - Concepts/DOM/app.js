44bb8c9576df48ad2ba84938264c882969776908
Miguel-T24 <miguelt2296@gmail.com>
2023-02-16 03:11:30 -0500

    Add DOM Manipulation

---
+++ Javascript - Concepts/DOM/app.js
@@ -0,0 +1,18 @@
+// let element;
+// element = document;
+// element = document.head;
+// element = document.title;
+
+// console.log(element);
+
+
+console.log(window);
+
+let element = document.querySelector('h2');
+console.log(element);
+
+let elementStyle = document.styleSheets
+console.log(elementStyle);
+
+// how many links have my document
+console.log(document.links);
