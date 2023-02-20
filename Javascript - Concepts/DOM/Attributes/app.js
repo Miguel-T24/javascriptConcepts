ab27016a7fa140de2c24f751cfadc02625b0b42d
Miguel-T24 <miguelt2296@gmail.com>
2023-02-20 12:44:30 -0500

    Add DOM Manipulation Attributes

---
+++ Javascript - Concepts/DOM/Attributes/app.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const input = document.querySelector("input");
+console.log("Type: ",input.type);
+console.log("placeholder:",input.placeholder);
+// input.value = 'Hello';
+console.log("value: ",input.value);
