1a7d70a473a933a254542a710a7d4743db94b22a
Miguel-T24 <miguelt2296@gmail.com>
2023-02-20 20:59:17 -0500

    Add Mouse Events

---
+++ Javascript - Concepts/DOM/9. Events III - Mouse/app.js
@@ -0,0 +1,33 @@
+'use strict'
+
+function alertEvent(){
+    alert('You activate on click');
+}
+
+function onContextMenufn(){
+    alert("You activate right click");
+}
+
+function onDobleClickfn(){
+    alert("You activate Doble click");
+}
+
+function onMouseDownfn(){
+    alert("You activate on mouse down");
+}
+
+function onMouseEnterfn(){
+    alert("You activate on mouse enter");
+}
+
+function onMouseLeavefn(){
+    alert("You Activate on mouse leave");
+}
+
+function onMouseMovefn(){
+    alert("You activate on mouse move");
+}
+
+function onMouseUpfn(){
+    alert("You activate on mouse up");
+}
